7521360c4d9638beba2e522c2e909b0e1a7de74e
Riste Poposki <rickyarpy@hotmail.com>
2018-09-17 20:51:05 +0200

    Add project files.

---
+++ AccessControlApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AccessControlApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
